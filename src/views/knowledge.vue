57e35eb6b18d7abcfeb86e9ab8c4886979f42b91
哲舟(胡周) <huzhou@ybm100.com>
2026-08-09 23:12:26 +0800

    筛选项组件封装

--- src/views/knowledge.vue
+++ src/views/knowledge.vue
@@ -5,12 +5,34 @@
         <el-button type="primary">新增</el-button>
       </template>
     </PageHeader>
+    <TableSearch :formItem="formItem" @search="handleSearch"></TableSearch>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue'
 import PageHeader from '@/components/PageHeader.vue';
+import TableSearch from '@/components/TableSearch.vue';
+const formItem = [
+  { comp: 'input', prop: 'title', label: '文章标题', placeholder: '请输入文章标题' },
+  { comp: 'select', prop: 'categoryId', label: '分类', placeholder: '请选择分类' },
+  {
+    comp: 'select', prop: 'status', label: '状态', placeholder: '请输入文章内容', options: [{
+      label: '草稿',
+      value: '0'
+    }, {
+      label: '已发布',
+      value: '1'
+    }, {
+      label: '已下线',
+      value: '2'
+    }]
+  }
+]
+
+const handleSearch = (formData) => {
+  console.log("查询参数", formData);
+}
 
 </script>
 
